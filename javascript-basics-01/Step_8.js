c2459fc827571694e0b18bf929264579de52594d
hajarmsheik <hajar.msheik@gmail.com>
2021-06-01 18:55:17 +0300

    if else

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,12 @@
+function userage(){   
+    var nb1=document.getElementById("age").value;
+    
+     if (nb1>18){
+         alert("You are over 18");
+     }
+      else
+      {  
+          alert("you are under 18");
+       
+     }
+}
