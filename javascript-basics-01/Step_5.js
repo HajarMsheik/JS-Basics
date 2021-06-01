14ee5819524c421d6866573e25ed7f87854b726e
hajarmsheik <hajar.msheik@gmail.com>
2021-06-01 15:06:43 +0300

    Simple math

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+
+function multiply(){   
+var nb1=document.getElementById("first_number").value;
+var nb2=document.getElementById("second_number").value;
+
+var result=nb1*nb2;
+alert("The result is "+result); 
+}
