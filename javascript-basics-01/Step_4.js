1a45f6827c3d761155cbdf28d6b9930f522d233c
hajarmsheik <hajar.msheik@gmail.com>
2021-06-01 14:46:11 +0300

    String Concatenation 2

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var name=prompt("Please,enter your name");
+var surname=prompt("Please,enter your surname");
+var city=prompt("Please,enter your city");
+alert("Hello, "+name +" "+surname +" "+"from"+" "+city); 
